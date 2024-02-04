fe311c211bfb335ffee4ee29f6573ecb38d1dba4
Khudoykulov <155818140+Khudkulov@users.noreply.github.com>
2024-02-04 16:15:43 +0500

    2

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ContactView
 app_name = 'main'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('contact/', ContactView.as_view(), name='contact')
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'main/index.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'main/contact.html'
